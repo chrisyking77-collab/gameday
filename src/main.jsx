4601f5371c74984ece713c1d1a6b0aae0150c601
Dwyer Eaves <dwyereaves@Dwyers-MacBook-Air-2.local>
2026-02-27 12:20:14 -0500

    add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Analytics } from '@vercel/analytics/react'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+)
